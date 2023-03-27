839c568d1bb5816b4d75ea36f8c91fb80f58eb2e
Largou Imane <imane.largou@student-cs.fr>
2023-03-27 16:07:46 +0200

    Install new python version

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # !/bin/sh
 
 sudo apt update -y
+sudo apt install python3.8 -y
 sudo apt install python3-pip -y
 sudo apt-get install -y --no-install-recommends openmpi-bin
 sudo apt-get install -y libopenmpi-dev
