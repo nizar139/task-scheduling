6d17732b48c8d80433d520f876f5c4922934a64b
William ROCHE <william.roche@student-cs.fr>
2023-03-28 16:30:55 +0200

    task scheduling sans fonction ordo

--- tasksscheduling.py
+++ tasksscheduling.py
@@ -12,42 +12,49 @@ def scheduling_parallel(priority_list, nb_cores):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-
-    list_split = split_list(priority_list, size)
+    
     if rank == 0: #master a le rank 0
+        list_split = split_list(priority_list, size)
         for i in range(1, size):
             comm.send(list_split[i], dest=i)
         scheduled_list = ordo(list_split[0], nb_cores)
         score = get_score_1(scheduled_list)
-        res = {'schedule' : scheduled_list,'score': score, 'rank': rank}
     else:
         sub_list = comm.recv(source=0)
         scheduled_list = ordo(sub_list, nb_cores)
         score = get_score_1(scheduled_list)
-        res = {'schedule' : scheduled_list,'score': score, 'rank': rank}
 
-    scheduled_dict = comm.gather(res, root=0)
+    relative_time = comm.gather(score, root=0)
+    if rank==0:
+        relative_time = np.cumsum(np.array(relative_time)) #relative_time[-1] est le score du schedule
+    comm.broadcast(relative_time)
+    if rank > 0:
+        time = relative_time[rank-1] #calcul des temps de fin de chaque schedule pour actualiser les temps start et end de chaque tache en fonction du rang du processeur qui les a ordonné
+        for x in scheduled_list:
+            for y in scheduled_list[x]:
+                y['start']+=time
+                y['end']+=time
+    scheduled_dict= comm.gather(scheduled_list, root=0)
     if rank == 0:
-        schedule = {}
-        
-        print(scheduled_dict)
-        relative_time = np.zeros(size) #calcul des temps de fin de chaque schedule pour actualiser les temps start et end de chaque tache en fonction du rang du processeur qui les a ordonné
-        for x in scheduled_dict:
-            relative_time[x['rank']]= x['score']
-        relative_time = np.cumsum(relative_time) #relative_time[-1] est le score du schedule
+        schedule ={i: [] for i in range(nb_cores)}
         for x in scheduled_dict: #actualisation des temps Start et End    
-            if x['rank']>0:
-                x['schedule']['Start'] +=relative_time[x['rank']-1]
-                x['schedule']['End'] +=relative_time[x['rank']-1]
-            schedule.update(x['schedule']) #merge chaque dictionnaire
+            for i in range(nb_cores):
+                schedule[i] += x[i]
         return schedule
 
 # %%
 def ordo(tasks, n_cores):
-    #calcule l'ordonnacement de la liste tasks
+    return None
+    #calcule l'ordonnancement de la liste tasks
 
-def get_score_1(l):
-    #calcule le score de la liste l
+def get_score_1(schedule):
+    score = 0
+    for core_list in schedule.values() :
+        if len(core_list)>0 :
+            new_score = core_list[-1]['end']
+            if new_score>score :
+                score = new_score
+    return score
 # %%
 #scheduling_parallel([1,2,3], 2)
 
